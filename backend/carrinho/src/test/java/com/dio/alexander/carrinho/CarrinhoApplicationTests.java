84bfee5b3489ecef0bc951d2866b9fa0d2f03239
Alexander Santos Andrade <al.praia@gmail.com>
2021-09-24 21:28:30 -0300

    Inicia estrutura do backend, com os micro serviços principais

---
+++ backend/carrinho/src/test/java/com/dio/alexander/carrinho/CarrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.alexander.carrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/catalogoproduto/src/test/java/com/dio/alexander/catalogoproduto/CatalogoprodutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.alexander.catalogoproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoprodutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
